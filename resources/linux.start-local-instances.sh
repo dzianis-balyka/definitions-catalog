bc35c5549460e07098ab8ea8b406f2bb0300f0d8
Dzianis Balyka <denikmail@gmail.com>
2019-04-02 11:31:38 +0300

    start script for linux

---
+++ resources/linux.start-local-instances.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+gnome-terminal -- /usr/local/etc/java-soft/apache-cassandra-3.11.4/bin/cassandra -f
+#TIMEOUT /T 30
+#REM
+#START "cqlsh-jg" /D C:\javasoft\apache-cassandra-3.11.4 .\bin\cqlsh.bat
+#REM START "es-jg" /D G:\javasoft\elasticsearch-6.2.chmod4\bin elasticsearch.bat
